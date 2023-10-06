b3ff9baa7cb0a6857944e68972975fc918e221b1
Elias Carvalho <eliascarvdev@gmail.com>
2023-10-06 16:39:54 -0300

    Add tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,30 @@
 using GeoDatasets
+using Meshes
 using Test
 
 @testset "GeoDatasets.jl" begin
-    # Write your tests here.
+  # @testset "gadm" begin
+  #   gtb = gadm("SVN", depth=1, ϵ=0.04)
+  #   @test length(gtb.geometry) == 12
+
+  #   gtb = gadm("QAT", depth=1, ϵ=0.04)
+  #   @test length(gtb.geometry) == 7
+
+  #   gtb = gadm("ISR", depth=1)
+  #   @test length(gtb.geometry) == 7
+  # end
+
+  @testset "inmetstations" begin
+    # automatic stations
+    gtb = inmetstations()
+    @test all(isequal("Automatica"), gtb.TP_ESTACAO)
+    @test gtb.geometry isa PointSet
+    @test embeddim(gtb.geometry) == 3
+
+    # manual stations
+    gtb = inmetstations(:manual)
+    @test all(isequal("Convencional"), gtb.TP_ESTACAO)
+    @test gtb.geometry isa PointSet
+    @test embeddim(gtb.geometry) == 3
+  end
 end
