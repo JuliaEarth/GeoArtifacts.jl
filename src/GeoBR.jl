85ec88697dd1425c17d171a69252604dcd9f2023
aybanda <ajaybandaru17@gmail.com>
2024-09-10 23:51:06 +0530

    altered code as per review and insight given

--- src/GeoBR.jl
+++ src/GeoBR.jl
@@ -1,60 +1,60 @@
-   module GeoBR
+module GeoBR
 
-   using HTTP
-   using JSON3
-   using GeoJSON
+using GeoIO
 
-   export municipality, state, country, region
+export municipality, state, country, region, readMunicipality
 
-   const BASE_URL = "https://raw.githubusercontent.com/ipeaGIT/geobr/master/data-raw"
+const BASE_URL = "https://raw.githubusercontent.com/ipeaGIT/geobr/master/data-raw"
 
-   function municipality(; code::Union{Int,Nothing}=nothing, year::Int=2010)
-       if isnothing(code)
-           error("Municipality code is required")
-       end
-       fetch_geobr_data("municipality", code, year)
-   end
+function municipality(code::Int, year::Int=2010)
+    return fetchGeobrData("municipality", code, year)
+end
 
-   function state(; code::Union{Int,Nothing}=nothing, year::Int=2010)
-       fetch_geobr_data("state", code, year)
-   end
+function readMunicipality(codeMuni::Int, year::Int=2010)
+    return municipality(codeMuni, year)
+end
 
-   function country(; year::Int=2010)
-       fetch_geobr_data("country", nothing, year)
-   end
+function state(code::Union{Int,Nothing}, year::Int=2010)
+    return fetchGeobrData("state", code, year)
+end
 
-   function region(; year::Int=2010)
-       fetch_geobr_data("region", nothing, year)
-   end
+function country(year::Int=2010)
+    return fetchGeobrData("country", nothing, year)
+end
 
-   function fetch_geobr_data(type::String, code::Union{Int,Nothing}, year::Int)
-       println("Debug: Entering fetch_geobr_data function")
-       println("Debug: type=$type, code=$code, year=$year")
-       code_str = isnothing(code) ? "all" : lpad(code, 7, '0')
-       base_url = "https://raw.githubusercontent.com/ipeaGIT/geobr/master/data-raw"
-       
-       # Try different URL patterns
-       urls = [
-           "$base_url/$year/$(type)_$code_str.geojson",
-           "$base_url/$(type)/$year/$(type)_$code_str.geojson",
-           "$base_url/$(type)_$code_str.geojson"
-       ]
-       
-       for (i, url) in enumerate(urls)
-           try
-               response = HTTP.get(url)
-               println("Debug: Response status for URL $i: $(response.status)")
-               if response.status == 200
-                   println("Debug: Successful response from URL $i")
-                   data = JSON3.read(String(response.body))
-                   return GeoJSON.FeatureCollection(data)
-               end
-           catch e
-               println("Debug: Error for URL $i: $e")
-           end
-       end
-       
-       error("Failed to fetch data: File not found for $type, code $code, year $year")
-   end
+function region(code::Union{Int,Nothing}, year::Int=2010)
+    return fetchGeobrData("region", code, year)
+end
 
-   end # module
+function fetchGeobrData(type::String, code::Union{Int,Nothing}, year::Int)
+    codeStr = isnothing(code) ? "all" : lpad(code, 7, '0')
+    
+    
+    # Try different URL patterns
+    urls = [
+        "$(BASE_URL)/$year/$(type)_$codeStr.geojson",
+        "$(BASE_URL)/$(type)/$year/$(type)_$codeStr.geojson",
+        "$(BASE_URL)/$(type)_$codeStr.geojson"
+    ]
+    
+    for url in urls
+        try
+            return GeoIO.read(url)
+        catch
+            # Continue to the next URL if there's an error
+        end
+    end
+    
+    # If all URLs fail, try fetching data for all entities of the given type
+    if !isnothing(code)
+        try
+            return fetchGeobrData(type, nothing, year)
+        catch
+            # If that also fails, throw an error
+        end
+    end
+    
+    error("Failed to fetch data: File not found for $type, code $code, year $year")
+end
+
+end # module
